95f733669de3d3b9b8ce914fa3ac70964f11004a
brendaprallon <brendaprallon@gmail.com>
2016-11-26 15:57:55 -0200

    commit settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+
 ]
